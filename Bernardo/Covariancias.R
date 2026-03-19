b8279988606075aba92cf2b0984448fb909319f1
Bernardo Ferreira <bernardofa31@gmail.com>
2026-03-19 11:14:51 -0300

    Add files via upload

--- Bernardo/Covariancias.R
+++ Bernardo/Covariancias.R
@@ -1,3 +1,4 @@
+#Método ruim, horrível, quem fizer isso será convidado a se retirar da sala pelo lupércio
 covariancias <- function(x,y)
 {
   #O as.vector faz virar uma lista (?!?!?!?)
@@ -17,6 +18,44 @@ for(l in 1:4)
     matriz[l,c] <- covariancias(iris[l],iris[c])
   }
 }
+matriz
+cov(iris[-5])
+variancia_total <- sum(diag(matriz))
+variancia_generalizada <- det(matriz)
+
+#----------Outro método-----------------
+
+covariancia_matricial <- function(x,y)
+{
+  x <- as.vector(x)[[1]]
+  y <- as.vector(y)[[1]]
+  xbarra <- mean(x)
+  ybarra <- mean(y)
+  xcentrado <- x-xbarra
+  ycentrado <- y-ybarra
+  return(t(xcentrado)%*%(ycentrado)/(length(x)-1))
+}
+
+matriz <- matrix(0,nrow=4,ncol=4)
+
+for(l in 1:4)
+{
+  for(c in 1:4)
+  {
+    matriz[l,c] <- covariancia_matricial(iris[l],iris[c])
+  }
+}
+
+matriz
+cov(iris[-5])
+variancia_total <- sum(diag(matriz))
+variancia_generalizada <- det(matriz)
+
+#----------Outro método-----------------
+#Objetivamente melhor!
+x <- scale(iris[-5],center=TRUE,scale=FALSE)
+
+matriz <- t(x)%*%x/(ncol(x)-1)
 matriz
 cov(iris[-5])
 variancia_total <- sum(diag(matriz))
