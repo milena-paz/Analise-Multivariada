f235ef063efa6097826e316ea8a353c828210cde
Milena Paz <milenap4zfreitas@gmail.com>
2026-05-19 15:17:00 -0300

    Update 19-05.R

--- Milena/19-05.R
+++ Milena/19-05.R
@@ -1,8 +1,6 @@
-medidas <- read.table("~/Documentos/MILENA-ANALISE.MULTIVARIADA/T4-3.DAT",header=F)
+medidas <- read.table("~/T4-3.DAT",header=F)
 names(medidas) <- c(paste0("X",1:4),"dj2")
 medidas <- medidas[-5]
-tam <- dim(medidas)[1]
-vars <- dim(medidas)[2]
 library(nortest)
 #### VERIFICAR NORMALIDADE UNIVARIADA: ####
 # - QQ-PLOT
@@ -13,7 +11,7 @@ library(nortest)
 
 apply(medidas,2,function(x){
   qqnorm(scale(x),pch=c(rep(1,8),19,rep(1,20)))
-  ##linha 9 destacada
+  ##obs. 9 destacada
   abline(a=0,b=1,col="red")
   pvalores <- c(ad.test(x)$p.value,  shapiro.test(x)$p.value,  lillie.test(x)$p.value) |> round(4)
   mtext(paste0("AD: ",pvalores[1],"\n",
@@ -27,6 +25,8 @@ apply(medidas,2,function(x){
 #   50% -> Quantidade
 #   95% -> Outliers
 pares <- list(c(1,2),c(1,3),c(1,4),c(2,3),c(2,4),c(3,4))
+names(pares) <- c("12","13","14","23","24","34")
+#pairs() com destaque para as observaçoes 9 e 16
 pairs(medidas,pch=c(rep(1,8),17,rep(1,6),15,rep(1,13)))
 Xc <- scale(medidas,scale=F)
 
@@ -40,11 +40,13 @@ Sij <- function(i,j){#COVARIANCIAS
 dj2 <- function(ij){
   diag(Xc[,ij]%*%solve(Sij(ij[1],ij[2]))%*%t(Xc[,ij]))
 }
-dj2.1 <- dj2(pares[[1]])
-which(dj2.1 > qchisq(0.95,df=2))
+# dj2.1 <- dj2(pares[[1]])
+# which(dj2.1 > qchisq(0.95,df=2))
 
-sapply(pares,function(ij) #qui-quadrado p/ achar outliers
-  which(dj2(ij) > qchisq(0.95,df=2)))
+#qui-quadrado p/ identificar outliers
+out <-sapply(pares,function(ij){
+  which(dj2(ij) > qchisq(0.95,df=2))
+  })
 library(ellipse)
 #GRAFICO CENTRADO
 graficoC <- function(i,j){
@@ -55,9 +57,13 @@ graficoC <- function(i,j){
 sapply(pares,function(ij){
   graficoC(ij[1],ij[2])
   disp <- Xc[,ij]
+  points(disp[out[[paste0(ij,collapse = "")]],],pch=19,col="maroon")
   elip <- ellipse(cor(disp)[1,2],level=0.95,scale=apply(disp,2,sd),
-                  centre=apply(disp,2,mean))
-  lines(elip,col="red",lwd=2)
+                  centre=colMeans(disp))
+  lines(elip,col="blue4",lty=2,lwd=2)
+  elip50 <- ellipse(cor(disp)[1,2],level=0.5,scale=apply(disp,2,sd),
+                  centre=colMeans(disp))
+  lines(elip50,col="#515ECC",lty=2)
   })
 #### VERIFICAR NORMALIDADE TETRAVARIADA: ####
 # - QQ-PLOT CHISQ(4)
